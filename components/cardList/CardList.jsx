c51822a884179e5755dad3c5172a5e745889a8cc
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-08-22 15:07:44 +0700

    commit to push

--- components/cardList/CardList.jsx
+++ components/cardList/CardList.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+// import Image from "next/image";
+// import Link from "next/link";
+// import React from "react";
 
-const CardList = () => {
-  return <div>CardList</div>;
-};
+// const CardList = () => {
+//   return (
 
-export default CardList;
+//   )
+// };
+
+// export default CardList;
