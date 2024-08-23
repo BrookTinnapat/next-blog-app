c0416ed6590180d84568badb75a9311239f92ce3
Brook Tinnapat <53811329+BrookTinnapat@users.noreply.github.com>
2024-08-23 15:55:28 +0700

    fix: add pugblih button to write page

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -81,6 +81,11 @@ const WritePage = () => {
       </div>
 
       {/* Publish Button */}
+      <div className="absolute bottom-0 right-0">
+        <button className="absolute top-4 right-4 bg-green-700 text-white px-6 py-2 rounded-md hover:bg-green-600 transition duration-200 ">
+          Publish
+        </button>
+      </div>
     </div>
   );
 };
